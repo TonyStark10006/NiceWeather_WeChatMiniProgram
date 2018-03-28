2e234926c318ebd828a3ed70837ada2fd0dc1284
TonyStark10006 <stvofy@gmail.com>
2018-03-28 16:26:06 +0800

    update some files

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,7 @@
 const app = getApp();
 const thisPage = getCurrentPages;
-var num = 0
-var apiData = require('../../resource/data.js')
-var config = require('../../config.js')
+const apiData = require('../../resources/data.js')
+const config = require('../../config.js')
 
 // pages/index/index.js
 Page({
@@ -18,6 +17,7 @@ Page({
    */
   onLoad: function (options) {
     //var that = this
+    var data = {}
     wx.showLoading({
       title: '定位中...',
     })
@@ -32,15 +32,7 @@ Page({
         var longitude = res.longitude
         var speed = res.speed
         var accuracy = res.accuracy
-        console.log('纬度：' + latitude + "  纬度：" + longitude)
-        // wx.showLoading({
-        //   title: '经度:' +longitude
-        // })
-        //更新视图
-        this.setData({
-          longitude: res.longitude,
-          latitude: res.latitude
-        })
+        //console.log('纬度：' + latitude + "  纬度：" + longitude)
         //根据获取到的坐标查询城市代码
         wx.request({
           url: config.data.baiduUrl,
@@ -51,10 +43,71 @@ Page({
             pois: 1
           },
           success: (res) => {
-            this.setData({
-              city: res.data.result.addressComponent.city
-            })
             var city = res.data.result.addressComponent.city
+            //心知天气接口-今天天气
+            wx.request({
+              url: config.data.xinzhiRTWthUrl,
+              data: {
+                location: city,
+                ts: config.data.xinzhiTS,
+                uid: config.data.xinzhiUID,
+                sig: config.data.xinzhiEncrypted
+              },
+              success: (res) => {
+                //console.log(res.data.results[0].now);
+                this.data["city"] = city;
+                this.data["tem"] = res.data.results[0].now.temperature;
+                this.data["wea"] = res.data.results[0].now.text;
+
+                //心知天气接口-未来三天天气
+                wx.request({
+                  url: config.data.xinzhiDailyWthUrl,
+                  data: {
+                    location: city,
+                    ts: config.data.xinzhiTS,
+                    uid: config.data.xinzhiUID,
+                    sig: config.data.xinzhiEncrypted,
+                    start: 0,
+                    days: 5
+                  },
+                  success: (res) => {
+                    //console.log(res.data.results[0].daily)
+                    var forecast = res.data.results[0].daily
+                    for (var i = 0; i < forecast.length; i++) {
+                    this.data["t" + (i + 1)] = //forecast[i].date + " " +
+                      forecast[i].text_day + " "
+                      + forecast[i].low + "℃-"
+                      + forecast[i].high + "℃ "
+                      //+ forecast[i].wind_direction_degrdd + " "
+                      + forecast[i].wind_direction
+                    }
+
+                    //心知天气接口-生活提示
+                    wx.request({
+                      url: config.data.xinzhiSuggestionUrl,
+                      data: {
+                        location: city,
+                        ts: config.data.xinzhiTS,
+                        uid: config.data.xinzhiUID,
+                        sig: config.data.xinzhiEncrypted
+                      },
+                      success: (res) => {
+                        //console.log(res.data.results[0].suggestion)
+                        var suggestion = res.data.results[0].suggestion;
+                        this.data["tips"] = "可穿" + suggestion.dressing.brief + "衣服，感冒" +
+                          suggestion.flu.brief + "，紫外线" + suggestion.uv.brief;
+
+                        //更新视图
+                        this.setData(this.data)
+                      }
+                    })
+
+                  }
+                })
+              }
+            })
+
+            // 个人接口
             // 根据获得城市名称查询天气
             // wx.request({
             //   url: '',
@@ -66,27 +119,33 @@ Page({
             //   }
             // })
             //更新视图
-            var data = {}
-            for(var i = 0; i < apiData.data.data.forecast.length; i++) {
-              data["t" + (i + 1)] = apiData.data.data.forecast[i].date + " "
-                + apiData.data.data.forecast[i].type + " "
-                + apiData.data.data.forecast[i].high + " "
-                + apiData.data.data.forecast[i].low + " "
-                //+ apiData.data.data.forecast[0].fengli.nodevalue + " "
-                + apiData.data.data.forecast[i].fengxiang
-            }
-            this.setData(data)
-            this.setData({
-              tips: apiData.data.data.ganmao,
-              tem: apiData.data.data.wendu
-            })
-            console.log(res.data.result)
-            console.log(apiData.data.data.forecast[0].date)
+            // var data = {}
+            // for(var i = 0; i < apiData.data.data.forecast.length; i++) {
+            //   data["t" + (i + 1)] = apiData.data.data.forecast[i].date + " "
+            //     + apiData.data.data.forecast[i].type + " "
+            //     + apiData.data.data.forecast[i].high + " "
+            //     + apiData.data.data.forecast[i].low + " "
+            //     //+ apiData.data.data.forecast[0].fengli.nodevalue + " "
+            //     + apiData.data.data.forecast[i].fengxiang
+            // }
+            // this.setData(data)
+            // this.setData({
+            //   tips: apiData.data.data.ganmao,
+            //   tem: apiData.data.data.wendu,
+            //   city: city
+            // })
+            // console.log(apiData.data.data.forecast[0].date)
+
+
+
+            //console.log(res.data.result)
+            //console.log("心知key加密后结果" + config.data.xinzhiEncrypted)
             wx.hideLoading()
           }
         })
       }
     })
+            console.log(this.data)
   },
 
   /**
