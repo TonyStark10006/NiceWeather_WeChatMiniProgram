81d6557acd8b6b555e88112d6fe4cd6921902f8a
TonyStark10006 <stvofy@gmail.com>
2018-08-13 18:13:58 +0800

    update version message

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //app.js
 App({
   globalData: {
-    version: "2.1.0 (180717)",
+    version: "2.2.1 (180813)",
     credit: "感谢心知天气、中国天气网、百度地图提供数据。\n数据仅供参考",
     copyRight: "Copyright © 2018 Vofy."
   }
